fb16908cd36dde83ffba6ee8a0e985403fbe38e0
soufiane-ekouines <soufianeekouines@gmail.com>
2022-04-23 16:24:51 +0000

    first commit

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/Home.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/normalize.css') }}">
+    <title>Ekouines</title>
+</head>
+<body>
+    <!-- start header  -->
+    <div class="header">
+        <!-- start container  -->
+        <div class="container">
+                <a href="#" class="logo">
+                    Ekouines
+                </a>
+            <ul class="Links">
+                <li> <a href="#articles"> Articles </a> </li>
+                <li> <a href="#gallery"> Gallery </a> </li>
+                <li> <a href="#features"> Features </a> </li>
+                <li> <a href="#"> Other Links </a> </li>
+            </ul>
+        </div>
+        <!-- End container  -->
+    </div>
+    <!-- End header  -->
+
+    <!-- start landing -->
+
+    <div class="landing">
+        <div class="container">
+            <div class="text">
+                <h1>Welcome, To Ekouines World</h1>
+                <p>
+                    Here Iam gonna share everything about my life. Books Iam reading, Games Iam Playing, Stories and Events
+                </p>
+            </div>
+            <div class="image">
+                <img src="{{asset('image/landing-image.png')}}" alt="">
+            </div>
+        </div>
+        <!-- <a href="#articles" class="go-down"></a> -->
+    </div>
+    <!-- end landing  -->
+</body>
+</html>
