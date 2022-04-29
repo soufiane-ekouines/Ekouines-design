7813ac3c10fcbf8b9cc79942048089f305f5ef78
soufiane-ekouines <soufianeekouines@gmail.com>
2022-04-29 03:53:31 +0000

    end services

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -503,5 +503,56 @@
         </div>
     </div>
     {{-- End TEAM MEMBERS --}}
+
+    {{-- Start services --}}
+    <div class="services" id="services">
+        <h2 class="main-title">Services</h2>
+        <div class="container">
+          <div class="box">
+            <i class="fas fa-user-shield fa-4x"></i>
+            <h3>Security</h3>
+            <div class="info">
+              <a href="#">Details</a>
+            </div>
+          </div>
+          <div class="box">
+            <i class="fas fa-tools fa-4x"></i>
+            <h3>Fixing Issues</h3>
+            <div class="info">
+              <a href="#">Details</a>
+            </div>
+          </div>
+          <div class="box">
+            <i class="fas fa-map-marked-alt fa-4x"></i>
+            <h3>Location</h3>
+            <div class="info">
+              <a href="#">Details</a>
+            </div>
+          </div>
+          <div class="box">
+            <i class="fas fa-laptop-code fa-4x"></i>
+            <h3>Coding</h3>
+            <div class="info">
+              <a href="#">Details</a>
+            </div>
+          </div>
+          <div class="box">
+            <i class="fas fa-palette fa-4x"></i>
+            <h3>Security</h3>
+            <div class="info">
+              <a href="#">Design</a>
+            </div>
+          </div>
+          <div class="box">
+            <i class="fas fa-bullhorn fa-4x"></i>
+            <h3>Marketing</h3>
+            <div class="info">
+              <a href="#">Details</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    {{-- End servises --}}
 </body>
 </html>
